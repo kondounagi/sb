35df328edd98d474958611814f770bf1b194c4fa
Rist019 <rist019@adminnoMacBook-Air.local>
2019-10-19 20:51:47 +0900

    update sample

--- sample_sub.py
+++ sample_sub.py
@@ -1,18 +1,29 @@
 import datetime
-def trans_date(day):
-    daytime =datetime.datetime.strptime(day, '%Y-%m-%d')
-    return daytime
-def fix_sumple_sub(sub_df):
-    sub_df["date"] = sub_df["date"].apply(trans_date)
-def get_week_day(sub_df):
-    def nun_to_mon(num):
+
+
+def fix_sample_sub(sub_df, store_info_df):
+    def trans_date(day):
+        daytime =datetime.datetime.strptime(day, '%Y-%m-%d')
+        return daytime
+    def sample_sub_date(sub_df):
+        sub_df["date"] = sub_df["date"].apply(trans_date)
+        return sub_df
+    def get_week_day(sub_df):
+        def sun_to_mon(num):
+            num -= 1
+            return num%6
+        sub_df["weekday"] = sub_df["date"].weekday().apply(sun_to_mon)
+        return sub_df
         
-        return num%6
-    
 
-def merge_store_info(sub_df,store_info):
-    sub_df = pd.merge(sub_df,store_info, on = "store_merchant_id", how = "left")
+    def merge_store_info(sub_df,store_info):
+        sub_df = pd.merge(sub_df,store_info, on = "store_merchant_id", how = "left")
 
+        return sub_df
+        
+    sub_df = sample_sub_date(sub_df)
+    sub_df = get_week_day(sub_df)
+    sub_df = merge_store_info(sub_df, store_info_df)
     return sub_df
 
 
