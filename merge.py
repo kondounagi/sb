6c4c54ce4b4a92e7d5522a93c53b6a656c593d08
Rist019 <rist019@adminnoMacBook-Air.local>
2019-10-19 16:16:12 +0900

    add group window mean

--- merge.py
+++ merge.py
@@ -75,3 +75,13 @@ def rolling_encode(df, target_col, oneday_num, w_size = 1):
     df[new_col_name] = tmp
     df[new_col_name].iloc[:oneday_num] = df[new_col_name].iloc[oneday_num].copy()
     return df
+
+def get_group_window_mean(df, group_col ,target_col,  w_size = 2 ):
+    new_col_name = target_col + "_group_win_mean"
+    df_gb = df[[group_col, target_col]].copy()
+    for_shift = df.groupby(group_col)[target_col].shift(1).isna()
+    df_gb = df_gb.groupby(group_col)[target_col].rolling(w_size).mean().shift(1).reset_index().sort_values("level_1").reset_index(drop = True)
+    df_gb.loc[for_shift, :] = np.nan
+    df[new_col_name] = df_gb[target_col]
+    
+    return df
